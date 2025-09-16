de9cab9c3b45021c4a7717a17ec8d5c442eaeff5
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-16 13:40:25 +0000

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,8 +1,30 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js").then(
-      reg => console.log("ServiceWorker OK:", reg.scope),
-      err => console.error("ServiceWorker FAIL:", err)
-    );
-  });
-}
+const CACHE = 'multitool-offline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './sw-register.js',
+  './manifest.json',
+  './js/suncalc.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
